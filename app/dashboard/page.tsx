67e7a00c6536fda39709ec3d08aa1219154da9a5
juanequex <ramon.g.uz37@gmail.com>
2024-10-08 18:13:54 -0600

    enable revenue charts

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,12 +1,18 @@
-import React from 'react'
+import RevenueChart from '@/app/ui/dashboard/revenue-chart';
+import { lusitana } from '@/app/ui/fonts';
+import { fetchRevenue } from '@/app/lib/data';
 
-const Page = () => {
+export default async function Page() {
+  const revenue = await fetchRevenue();
 
   return (
-    <div>
-      This is the dashboard
-    </div>
-  )
+    <main>
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Dashboard
+      </h1>
+      <div className="mt-6 grid grid-cols-1 gap-6 md:grid-cols-4 lg:grid-cols-8">
+        <RevenueChart revenue={revenue}  />
+      </div>
+    </main>
+  );
 }
-
-export default Page
